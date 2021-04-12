e204f2b682ccb5b41f7ee611a6787e3657113e36
Rick Rothenberg <rrothenb@Mac-mini.local>
2021-04-12 03:38:12 -0600

    Add script to assemble frame from subframes

---
+++ assemble.sh
@@ -0,0 +1,7 @@
+convert 0000.01.png 0000.02.png 0000.03.png 0000.04.png 0000.05.png 0000.06.png +append row1.png
+convert 0000.07.png 0000.08.png 0000.09.png 0000.10.png 0000.11.png 0000.12.png +append row2.png
+convert 0000.13.png 0000.14.png 0000.15.png 0000.16.png 0000.17.png 0000.18.png +append row3.png
+convert 0000.19.png 0000.20.png 0000.21.png 0000.22.png 0000.23.png 0000.24.png +append row4.png
+convert 0000.25.png 0000.26.png 0000.27.png 0000.28.png 0000.29.png 0000.30.png +append row5.png
+convert 0000.31.png 0000.32.png 0000.33.png 0000.34.png 0000.34.png 0000.34.png +append row6.png
+convert row1.png row2.png row3.png row4.png row5.png row6.png -append 0000.png
