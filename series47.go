7df8063eb9cacecb5bf76bb3cc46e51913876e18
Rick Rothenberg <rrothenb@yahoo.com>
2022-02-20 21:04:26 -0600

    Series 47 final?

--- series47.go
+++ series47.go
@@ -414,12 +414,12 @@ end_header
         </transform>
 
         <sampler type="independent">
-            <integer name="sample_count" value="1024"/>
+            <integer name="sample_count" value="256"/>
         </sampler>
 
         <film type="hdrfilm" id="film">
-            <integer name="width" value="3000"/>
-            <integer name="height" value="2250"/>
+            <integer name="width" value="2600"/>
+            <integer name="height" value="1950"/>
             <rfilter type="box"/>
         </film>
     </sensor>
