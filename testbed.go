6b421d6c1037b52226f52eb33ba2fa7fb418955c
Rick Rothenberg <rrothenb@yahoo.com>
2022-10-29 01:55:20 -0500

    Testbed with some partially implemented logic for curving surface

---
+++ testbed.go
@@ -0,0 +1,309 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"math"
+	"os"
+	"text/template"
+	"encoding/binary"
+	"bufio"
+
+	"github.com/hunterloftis/pbr/pkg/geom"
+)
+
+type MeshType struct {
+	NumVertices int
+	NumFaces    int
+}
+
+var sin = math.Sin
+var cos = math.Cos
+var pow = math.Pow
+var sqrt = math.Sqrt
+var pi = math.Pi
+var abs = math.Abs
+var min = math.Min
+var max = math.Max
+var atan = math.Atan
+func sign(x float64) float64 {
+	if x < 0 {
+		return -1
+	} else {
+		return 1
+	}
+}
+func spow(x, y float64) float64 {
+	return sign(x)*pow(abs(x), y)
+}
+
+func subtexture2(u, v, t float64) float64 {
+	return sin(7*u+subtexture3(u, v, t))+sin(5*v+subtexture3(u, v, t))
+}
+
+func subtexture3(u, v, t float64) float64 {
+	return sin(2*u+3*v)
+}
+
+func texture(u, v, t float64) float64 {
+	u = u/2 + pi/2
+	v = v/2 + pi/2
+	return sin(3*u-3*v+subtexture2(3*u, 3*v, t))*sin(3*u+3*v+subtexture2(3*v, 3*u, t))
+}
+
+func radius(u, v, t float64) float64 {
+	return .25*spow(cos(u+sin(v)*sin(u+5*sin(2*u+sin(5*v)*2*sin(3*u+sin(2*u+sin(13*u)))))+sin(3*v)*3*sin(2*u)), .25)
+}
+
+func pushdown(x, n float64) float64 {
+	return pow(x/2+.5, n)*2-1
+}
+
+type SLR2 struct {
+	Width  float64
+	Height float64
+	Lens   float64
+	FStop  float64
+	Focus  float64
+
+	trans    *geom.Mtx
+	position geom.Vec
+	target   geom.Vec
+}
+
+var zAxis = geom.Dir{0, 0, 1}
+
+// NewSLR constructs a new camera with 35mm sensor full-frame / 50mm lens defaults.
+func NewSLR2() *SLR2 {
+	s := &SLR2{
+		Width:    0.048,
+		Height:   0.027,
+		Lens:     0.050, // 50mm focal length
+		FStop:    4,
+		Focus:    1,
+		position: geom.Vec{0, 0, 0},
+		target:   geom.Vec{0, 0, -5},
+	}
+	s.transform()
+	return s
+}
+
+// LookAt orients a Camera to face a target.
+func (s *SLR2) LookAt(target geom.Vec) *SLR2 {
+	s.target = target
+	s.transform()
+	return s
+}
+
+// MoveTo moves a Camera to a position given by x, y, and z coordinates.
+func (s *SLR2) MoveTo(pos geom.Vec) *SLR2 {
+	s.position = pos
+	s.transform()
+	return s
+}
+
+func LookMatrix(o geom.Vec, to geom.Vec) *geom.Mtx {
+	f, _ := o.Minus(to).Unit() // forward
+	r, _ := zAxis.Cross(f)     // right
+	u, _ := f.Cross(r)         // up
+	orient := geom.NewMat(
+		r.X, u.X, f.X, 0,
+		r.Y, u.Y, f.Y, 0,
+		r.Z, u.Z, f.Z, 0,
+		0, 0, 0, 1,
+	)
+	return geom.Shift(o).Mult(orient)
+}
+
+func (s *SLR2) transform() {
+	s.trans = LookMatrix(s.position, s.target)
+}
+
+func (s *SLR2) invisible(point geom.Vec) bool {
+	cameraSpaceTransform := s.trans.Inverse()
+	projectedPoint := cameraSpaceTransform.MultPoint(point)
+	//fmt.Printf("\npoint: %#v\nprojectedPoint: %#v\ncameraSpaceTransform: %#v\n", point, projectedPoint, cameraSpaceTransform)
+	factor := .01
+	aspectRatio := s.Width/s.Height
+	if projectedPoint.X < projectedPoint.Z*factor*aspectRatio || projectedPoint.X > -projectedPoint.Z*factor*aspectRatio {
+		return true
+	}
+	if projectedPoint.Y < projectedPoint.Z*factor || projectedPoint.Y > -projectedPoint.Z*factor {
+		return true
+	}
+	return false
+	if projectedPoint.Z > 0.0 {
+		return true
+	}
+	return false
+	if projectedPoint.Z < -s.position.Len() {
+		return true
+	}
+	if projectedPoint.X < projectedPoint.Z/2 || projectedPoint.X > -projectedPoint.Z/2 {
+		return true
+	}
+	if projectedPoint.Y < projectedPoint.Z/2 || projectedPoint.Y > -projectedPoint.Z/2 {
+		return true
+	}
+	return false
+}
+
+/** TODO
+end up with vectors in xz and yz planes and add them together to figure out direction
+regardless of X and Y circle locs, effect should be 0 at center
+it might actually be easier to computer if effect is 0 at edges
+hard part may be converting from X or Y to angle on sphere
+I like the edges being fixed 0 or maybe just the corners
+so, only specify numbers for x and for y that just can't be zero but that determine the percent
+range of the scaled u and v such that the corners are at -1,-1 and 1,1
+0 would be 50% and infinity would be 0%.  at 1, what would the math be? It would be 1 below
+the line, but the radius would be higher.
+r = sqrt (1+x^2)
+angle = arctan(1/x)
+ */
+func uv2xyz(u, v, t float64, radius func(u, v, t float64) float64) geom.Vec {
+	//a := radius(u, v, t)
+	xCenter := .0001
+	xRadius := pow(1+pow(xCenter, 2), .5)
+	angle := atan(1/xCenter)
+	u2 := -angle + u/pi*angle
+	xVector := geom.Vec{sin(u2), 0, cos(u2)}.Scaled(xRadius).Plus(geom.Vec{xCenter, v/pi - 1, 0})
+	return xVector
+	/*
+	return geom.Vec{
+		sin(u2)*xRadius,
+		v/pi - 1,
+		.25*a+pow(sin(u/2), .5)+pow(sin(v/2), .5),
+	}
+
+	 */
+}
+
+func index2radians(index float64, n int) float64 {
+	return index / float64(n) * pi * 2
+}
+
+func uvIndexToNormal(uIndex, vIndex, n int, t float64) *geom.Dir {
+	left := uv2xyz(index2radians(float64(uIndex)-.1, n), index2radians(float64(vIndex), n), t, radius)
+	right := uv2xyz(index2radians(float64(uIndex)+.1, n), index2radians(float64(vIndex), n), t, radius)
+	up := uv2xyz(index2radians(float64(uIndex), n), index2radians(float64(vIndex)+.1, n), t, radius)
+	down := uv2xyz(index2radians(float64(uIndex), n), index2radians(float64(vIndex)-.1, n), t, radius)
+	normal, _ := left.Minus(right).Cross(up.Minus(down)).Unit()
+	return &normal
+}
+
+func renderSurfaces(frameNumber int, pixels int, maxSubdivisions int, dt float64, desiredTriangles int) {
+	t := float64(frameNumber) * dt
+	cameraLoc := geom.Vec{0, 0, 9}
+	focusPoint := geom.Vec{0, 0, 0}
+	c := NewSLR2().MoveTo(cameraLoc).LookAt(focusPoint)
+	c.FStop = 64
+	distance := cameraLoc.Minus(focusPoint).Len() - c.Lens
+	n := int(float64(pixels) / distance * 3)
+	if n > maxSubdivisions {
+		n = maxSubdivisions
+	}
+	if desiredTriangles > 0 {
+		numTriangles := 0
+		for uIndex := 0; uIndex <= 500; uIndex++ {
+			for vIndex := 0; vIndex <= 500; vIndex++ {
+				vertex := uv2xyz(index2radians(float64(uIndex), 500), index2radians(float64(vIndex), 500), t, radius)
+				if !c.invisible(vertex) {
+					numTriangles++
+				}
+			}
+		}
+		fmt.Println(numTriangles)
+		n = int(sqrt(float64(desiredTriangles)/float64(numTriangles*2)) * 500)
+	}
+	fmt.Printf("distance from center: %v, distance from focal point: %v, n: %v\n", cameraLoc.Len(), distance, n)
+	vertexIndicies := make([][]int32, n+1)
+	numVerticies := 0
+	plyDataPath := fmt.Sprintf("testbed.data.ply")
+	plyData, _ := os.Create(plyDataPath)
+	PlyDataBuffered := bufio.NewWriter(plyData)
+	for uIndex := 0; uIndex <= n; uIndex++ {
+		vertexIndicies[uIndex] = make([]int32, n+1)
+		for vIndex := 0; vIndex <= n; vIndex++ {
+			vertex := uv2xyz(index2radians(float64(uIndex), n), index2radians(float64(vIndex), n), t, radius)
+			if c.invisible(vertex) {
+				vertexIndicies[uIndex][vIndex] = -1
+				continue
+			}
+			normal := uvIndexToNormal(uIndex, vIndex, n, t)
+			vertexIndicies[uIndex][vIndex] = int32(numVerticies)
+			numVerticies++
+			binary.Write(PlyDataBuffered, binary.LittleEndian, float32(vertex.X))
+			binary.Write(PlyDataBuffered, binary.LittleEndian, float32(vertex.Y))
+			binary.Write(PlyDataBuffered, binary.LittleEndian, float32(vertex.Z))
+			binary.Write(PlyDataBuffered, binary.LittleEndian, float32(normal.X))
+			binary.Write(PlyDataBuffered, binary.LittleEndian, float32(normal.Y))
+			binary.Write(PlyDataBuffered, binary.LittleEndian, float32(normal.Z))
+			binary.Write(PlyDataBuffered, binary.LittleEndian, float32(index2radians(float64(uIndex), n)/pi/2))
+			binary.Write(PlyDataBuffered, binary.LittleEndian, float32(index2radians(float64(vIndex), n)/pi/2))
+		}
+	}
+	numFaces := 0
+	for vIndex := 0; vIndex < n; vIndex++ {
+		for uIndex := 0; uIndex < n; uIndex++ {
+			topRight := vertexIndicies[uIndex][vIndex]
+			topLeft := vertexIndicies[uIndex+1][vIndex]
+			botRight := vertexIndicies[uIndex][vIndex+1]
+			botLeft := vertexIndicies[uIndex+1][vIndex+1]
+			if topRight == -1 || topLeft == -1 || botRight == -1 || botLeft == -1 {
+				continue
+			}
+			if vIndex != 0 {
+				binary.Write(PlyDataBuffered, binary.LittleEndian, byte(3))
+				binary.Write(PlyDataBuffered, binary.LittleEndian, topRight)
+				binary.Write(PlyDataBuffered, binary.LittleEndian, botLeft)
+				binary.Write(PlyDataBuffered, binary.LittleEndian, topLeft)
+				numFaces++
+			}
+			if vIndex != n-1 {
+				binary.Write(PlyDataBuffered, binary.LittleEndian, byte(3))
+				binary.Write(PlyDataBuffered, binary.LittleEndian, topRight)
+				binary.Write(PlyDataBuffered, binary.LittleEndian, botRight)
+				binary.Write(PlyDataBuffered, binary.LittleEndian, botLeft)
+				numFaces++
+			}
+		}
+	}
+
+	fmt.Println("Mitsuba!")
+	PlyDataBuffered.Flush()
+	tmpl, _ := template.New("some template").Parse(`
+ply
+format binary_little_endian 1.0
+element vertex {{ .NumVertices }}
+property float32 x
+property float32 y
+property float32 z
+property float32 nx
+property float32 ny
+property float32 nz
+property float32 u
+property float32 v
+element face {{ .NumFaces }}
+property list uint8 int32 vertex_index
+end_header
+`)
+	plyHeaderPath := fmt.Sprintf("testbed.header.ply")
+	plyHeader, _ := os.Create(plyHeaderPath)
+	mesh := MeshType{}
+	mesh.NumVertices = numVerticies
+	mesh.NumFaces = numFaces
+	tmpl.Execute(plyHeader, mesh)
+}
+
+func main() {
+	frame := flag.Int("frame", 0, "Specify frame")
+	pixels := flag.Int("pixels", 256, "Specify height and width of generated image")
+	maxSubdivisions := flag.Int("maxsubdivisions", 1000, "Max subdivisions")
+	maxFrames := flag.Int("maxframes", 64, "Max frames")
+	desiredTriangles := flag.Int("desiredtriangles", 0, "The desired number of triangles to render")
+	flag.Parse()
+	fmt.Printf("frame: %v, pixels: %v, maxSubdivisions: %v, maxFrames: %v\n", *frame, *pixels, *maxSubdivisions, *maxFrames)
+	dt := pi * 2 / float64(*maxFrames)
+	renderSurfaces(*frame, *pixels, *maxSubdivisions, dt, *desiredTriangles)
+}
