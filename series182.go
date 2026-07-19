bdecf1b6ddc2dd7c506634b219b558bbc27faf76
Rick Rothenberg <rrothenb@yahoo.com>
2026-07-19 14:30:56 +0200

    Final

--- series182.go
+++ series182.go
@@ -1009,11 +1009,11 @@ end_header
             <lookat origin="{{ .Camera.X }}, {{ .Camera.Y }}, {{ .Camera.Z }}" target="{{ .LookAt.X }}, {{ .LookAt.Y }}, {{ .LookAt.Z }}" up="0, 1, 0"/>
         </transform>
         <sampler type="multijitter">
-            <integer name="sample_count" value="100"/>
+            <integer name="sample_count" value="2070"/>
         </sampler>
         <film type="hdrfilm" id="film">
-            <integer name="width" value="1280"/>
-            <integer name="height" value="1280"/>
+            <integer name="width" value="3072"/>
+            <integer name="height" value="3072"/>
             <rfilter type="gaussian"/>
         </film>
     </sensor>
